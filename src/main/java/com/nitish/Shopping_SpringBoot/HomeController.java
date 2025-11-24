d2b90a68d2788e540f49b730a32f5ee70c93b391
NitishMishra0 <nitishmishrani@gmail.com>
2025-11-24 19:12:42 +0530

    Initial web pages (home and login)

---
+++ src/main/java/com/nitish/Shopping_SpringBoot/HomeController.java
@@ -0,0 +1,15 @@
+package com.nitish.Shopping_SpringBoot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String greet(){
+        return "Welcome to Nitish Homepage!!";
+    }
+
+}
